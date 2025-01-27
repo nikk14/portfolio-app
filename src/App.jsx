a6264d51cba6116929c47315beb3e5b72fe64ea1
nikk14 <neeleshkhandey1818@gmail.com>
2025-01-27 22:53:55 +0530

    Link added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
-
+import { Routes, Route, Link } from 'react-router'
+import Home from './components/Home'
+import About from './components/About'
 function App() {
 
   return (
     <>
-         <h1 className="text-3xl font-bold underline">      Hello world!    </h1>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
   )
 }
